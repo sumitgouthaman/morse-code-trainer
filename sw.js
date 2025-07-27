1c18029872c0cf9653cb073e1541929315263b1c
Sumit Gouthaman <sumitgt007@gmail.com>
2025-07-27 13:12:26 -0700

    Some more service worker changes.

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-const CACHE_NAME = 'morse-trainer-v1';
+// Increment the CACHE_VERSION to trigger a cache update when any of the cached files change.
+const CACHE_VERSION = 'v2';
+const CACHE_NAME = `morse-trainer-${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/js/main.js',
+  '/js/char-to-morse.js',
+  '/js/morse-to-char.js',
+  '/js/sound-to-char.js',
+  '/js/learn.js',
+  '/js/settings.js',
+  '/js/statistics.js',
+  '/js/morse-code.js',
+  '/js/ui.js',
+  '/js/session-tracker.js',
+  '/html/char-to-morse.html',
+  '/html/morse-to-char.html',
+  '/html/sound-to-char.html',
+  '/html/learn.html',
   '/favicon.svg',
   '/icon-192.png',
   '/icon-512.png',
@@ -40,10 +55,36 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Fetch event - network first, then cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
-      .catch(() => caches.match(event.request))
+      .then(response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request)
+          .then(response => {
+            // Cache hit - return response
+            if (response) {
+              return response;
+            }
+          });
+      })
   );
 });
